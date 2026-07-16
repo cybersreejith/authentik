6627143a3a1a3708d0dc4e9bbd80cc714c3088d3
Sreejith <hisreenair@gmail.com>
2026-07-16 10:11:42 -0500

    Test changes

--- web/src/elements/messages/Message.ts
+++ web/src/elements/messages/Message.ts
@@ -74,7 +74,7 @@ export class Message extends AKElement {
     }
 
     public willUpdate(changed: PropertyValues<this>) {
-        if ((changed.has("lifetime") && this.lifetime) || changed.has("onDismiss")) {
+        if (changed.has("lifetime") && this.lifetime) {
             this.#scheduleDismiss();
         }
     }
